695a09c5a9022f3ecaaf664a04237c992de4ee77
Thomas Lobjoie <thomaslobjoie@users.noreply.github.com>
2020-11-13 19:40:24 +0000

    Add the game sequences

--- main.ts
+++ main.ts
@@ -4,72 +4,102 @@ interface Point {
 }
 
 class SpaceInvader {
-    score: number = 0;
-    nbShoots: number = 1;
-    ship: Point = {x: 2, y: 4};
-    shoots: Point[];
-    invaders: Point[];
-    explosions: Point[];
-    gameSpeed = 100;
+    private status = 'init';
+    private level = 0;
+    private score = 0;
+    private nbShoots = 0;
+    private gameSpeed = 100;
+    private ship = {x: 2, y: 4};
+    private shoots: Point[];
+    private invaders: Point[];
+    private explosions: Point[];
+    
 
-    constructor() {
-        this.shoots = [];
-        this.invaders = [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 0}, {x: 3, y: 1}, {x: 4, y: 0}];
-    }
+    public constructor() {}
 
-    step() {
+    public step() {
         this.moveShoots();
         this.moveInvaders();
         this.detectExplosions();
         this.update();
+        this.checkGameStatus();
     }
 
-    shoot() {
+    public shoot() {
         this.shoots.push({x: this.ship.x, y: this.ship.y});
+        this.nbShoots++;
     }
 
-    moveShoots() {
+    public left () {
+        if (this.ship.x > 0) {
+            this.ship.x--;
+        }
+    }
+
+    public right () {
+        if (this.ship.x < 4) {
+            this.ship.x++;
+        }
+    }
+
+    public getScore () {
+        return this.score;
+    }
+
+    public getStatus () {
+        return this.status;
+    }
+    public getLevel() {
+        return this.level;
+    }
+
+    public nextLevel() {
+        this.level++;
+        this.ship = {x: 2, y: 4};
+        this.shoots = [];
+        this.invaders = [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 0}, {x: 3, y: 1}, {x: 4, y: 0}];
+        this.status = 'play';
+    }
+
+    public restartGame() {
+        this.level = 0;
+        this.nextLevel();
+    }
+
+    private moveShoots() {
         this.shoots = this.shoots.filter((shoot: Point) => shoot.y > 0);
         this.shoots.forEach((shoot: Point) => {
             shoot.y--;
         });
     }
 
-    moveInvaders() {
-        /*this.shoots = this.shoots.filter((shoot: Point) => shoot.y > 0);
-        this.shoots.forEach((shoot: Point) => {
-            shoot.y--;
-        });*/
-    }
+    private moveInvaders() {}
 
-    detectExplosions() {
+    private detectExplosions() {
         this.explosions = [];
         this.explosions = this.comparePoints(this.shoots, this.invaders, true);
         this.shoots = this.comparePoints(this.shoots, this.explosions, false);
         this.invaders = this.comparePoints(this.invaders, this.explosions, false);
     }
 
-    comparePoints(pointsA: Point[], pointsB: Point[], keep: boolean) {
+    private comparePoints(pointsA: Point[], pointsB: Point[], keep: boolean) {
         return pointsA.filter(pointA => 
             pointsB.some(pointB => pointA.x === pointB.x && pointA.y === pointB.y) === keep
         );
     }
 
-    left () {
-        // music.playTone(Note.G, 180);
-        if (this.ship.x > 0) {
-            this.ship.x--;
+    private checkGameStatus() {
+        this.score += this.explosions.length;
+        if(this.invaders.length === 0) {
+            if(this.level > 2) {
+                this.status = 'end_game';
+            } else {
+                this.status = 'end_level';
+            }
         }
     }
 
-    right () {
-        // music.playTone(Note.G, 180);
-        if (this.ship.x < 4) {
-            this.ship.x++;
-        }
-    }
-
-    update() {
+    private update() {
         basic.clearScreen();
         this.lightPoints([this.ship], 100);
         this.lightPoints(this.shoots, 50);
@@ -77,15 +107,12 @@ class SpaceInvader {
         this.lightPoints(this.explosions, 255);
     }
 
-    lightPoints(points: Point[], brightness: number) {
+    private lightPoints(points: Point[], brightness: number) {
         points.forEach((value: Point) => {
             led.plotBrightness(value.x, value.y, brightness);
         });
     }
 }
-
-music.setOnBoardSpeakerEnabled(false);
-music.setVolume(60);
 
 let spaceInvaderGame = new SpaceInvader();
 
@@ -98,14 +125,43 @@ input.onButtonPressed(Button.B, function () {
 });
 
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    spaceInvaderGame.shoot();
+    let gameStatus = spaceInvaderGame.getStatus();
+    switch(gameStatus) {
+        case 'init':
+            spaceInvaderGame.nextLevel();
+            break;
+        case 'play':
+            spaceInvaderGame.shoot();
+            break;
+        case 'end_level':
+            spaceInvaderGame.nextLevel();
+            break;
+        case 'end_game':
+            spaceInvaderGame.restartGame();
+            break;
+    }
 });
 
-/*input.onSound(DetectedSound.Loud, function () {
-  spaceInvaderGame.shoot();
-});*/
-
 basic.forever(function () {
-    spaceInvaderGame.step();
-    basic.pause(200);
+    let gameStatus = spaceInvaderGame.getStatus();
+    switch(gameStatus) {
+        case 'init':
+            basic.showIcon(IconNames.Ghost);
+            basic.pause(100);
+            break;
+        case 'play':
+            spaceInvaderGame.step();
+            basic.pause(200);
+            break;
+        case 'end_level':
+            basic.showNumber(spaceInvaderGame.getLevel());
+            basic.pause(100);
+            break;
+        case 'end_game':
+            basic.showIcon(IconNames.Skull);
+            basic.showString('Score: ' + spaceInvaderGame.getScore());
+            basic.showIcon(IconNames.Skull);
+            basic.pause(1000);
+            break;
+    }
 });
